111a3a1616e675c5d4733db1269b16679885e835
Abhishek <aabhishekbbhardwaj@gmail.com>
2022-05-29 05:38:31 +0530

    Java Server

---
+++ src/main/java/com/spring/crud/SpringbootCrudApplication.java
@@ -0,0 +1,15 @@
+package com.spring.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+
+@SpringBootApplication
+public class SpringbootCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCrudApplication.class, args);
+	}
+
+}
